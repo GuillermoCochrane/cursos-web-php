dd32b971a74a6916fd81a791430e806562b1242d
GuillermoCochrane <guilleac81@gmail.com>
2026-02-03 02:44:09 -0300

    Agregado ejemplo de formas imprimir texto

---
+++ midudev/clase 1/index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<style>
+  :root {
+    color-scheme: light dark;
+  }
+
+  body {
+    display: grid;
+    place-content: center;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+  }
+</style>
+
+<h1><!-- formas  de imprimir texto en php -->
+  <?php echo "Hola mundo <br/>"; ?>  
+  <?= "MI primera app <br/>"; ?>
+</h1>
