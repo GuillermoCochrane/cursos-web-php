176507b7f0ffbcddf9e19aaedce651b947fd375f
GuillermoCochrane <guilleac81@gmail.com>
2026-02-03 20:25:13 -0300

    Agregado ejemplo de modularizacion de secciones html

--- midudev/clase 2/index.php
+++ midudev/clase 2/index.php
@@ -13,51 +13,8 @@ similar a require y require_once, pero no devuelve error si el archivo no existe
  */
 $data = get_data(API_URL);
 $until_message = get_until_message($data["days_until"]);
+
+require_once "sections/head.php";
+require_once "sections/styles.php";
+require_once "sections/main.php"; 
 ?>
-
-<head>
-  <meta charset="UTF-8" />
-  <title>La próxima película de Marvel</title>
-  <meta name="description" content="La próxima película de Marvel" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css" />
-  <style>
-    :root {
-      color-scheme: light dark;
-    }
-
-    body {
-      display: grid;
-      place-content: center;
-    }
-
-    section {
-      display: flex;
-      justify-content: center;
-      text-align: center;
-    }
-
-    hgroup {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      text-align: center;
-    }
-
-    img {
-      margin: 0 auto;
-    }
-  </style>
-</head>
-<main>
-
-  <section>
-    <img src="<?= $data["poster_url"]; ?>" width="200" alt="Poster de <?= $data["title"]; ?>" style="border-radius: 16px" />
-  </section>
-
-  <hgroup>
-    <h3><?= $data["title"]; ?> - <?= $until_message; ?></h3>
-    <p>Fecha de estreno: <?= $data["release_date"]; ?></p>
-    <p>La siguiente es: <?= $data["following_production"]["title"]; ?></p>
-  </hgroup>
-</main>
