73057faeaa622334a5aec1ac8eab287ee0ff904e
GuillermoCochrane <guilleac81@gmail.com>
2026-02-04 04:39:06 -0300

    Agregado ejemplo de métodos estáticos

--- midudev/clase 2/classes.php
+++ midudev/clase 2/classes.php
@@ -44,13 +44,33 @@ class SuperHero{
     return "¡$this->name está en el nivel $this->level!";
   }
 
-  public function show_all(){
+  public function show_all(){ // devuelve un array con todas las propiedades y métodos de la clase
     return get_object_vars($this);
+  }
+
+    public static function randomHero(){ // método estático que devuelve un heroe aleatorio de la clase SuperHero
+    $names = ["Thor", "Spiderman", "Wolverine", "Ironman", "Hulk"];
+    $powers = [
+      ["Superfuerza", "Volar", "Rayos láser"],
+      ["Superfuerza", "Super agilidad", "Telarañas"],
+      ["Regeneración", "Superfuerza", "Garras de adamantium"],
+      ["Superfuerza", "Volar", "Rayos láser"],
+      ["Superfuerza", "Super agilidad", "Cambio de tamaño"],
+    ];
+    $planets = ["Asgard", "HulkWorld", "Krypton", "Tierra"];
+
+    $name = $names[array_rand($names)];
+    $power = $powers[array_rand($powers)];
+    $planet = $planets[array_rand($planets)];
+
+    return new self($name, $power, $planet);
   }
 }
 
-$hero = new SuperHero("Thor", ["Superfuerza", "Volar", "Relámpago"], "Asgard");
+/* $hero = new SuperHero("Thor", ["Superfuerza", "Volar", "Relámpago"], "Asgard");
 echo $hero->description();
 echo "\n";
-echo $hero->level_up();
+echo $hero->level_up(); */
+$hero = SuperHero::random(); // método estático de la clase SuperHero (podemos acceder a los métodos estaticos de una clase sin instanciarla)
+echo $hero->description(); // método publico de la instancia $hero de la clase SuperHero
 ?>
