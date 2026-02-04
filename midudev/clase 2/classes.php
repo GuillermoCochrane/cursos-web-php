cae4d8bdd382228b1392b79d1cc2b9fdeec9eb3a
GuillermoCochrane <guilleac81@gmail.com>
2026-02-04 04:04:46 -0300

    Agregado ejemplo de constructor de class

--- midudev/clase 2/classes.php
+++ midudev/clase 2/classes.php
@@ -2,26 +2,33 @@
 
 class SuperHero{
   // propiedades y métodos
-  public $name;
+  public $name; // publico: podemos acceder a o modificar desde fuera de la clase
   public $powers;
   public $planet;
+  private $level; // privado: solo podemos accederlo o modificarlo desde dentro de la clase, no desde fuera
+
+  // constructor, necesario para inicializar las propiedades
+  public function __construct($name, $powers, $planet){
+    $this->name = $name;
+    $this->powers = $powers;
+    $this->planet = $planet;
+    $this->level = 0;
+  }
 
   public function attack(){
     return "¡$this->name ataca con sus poderes!";
   }
 
   public function description(){
-    $powers = implode(", ", $this->powers);
-    return "$this->name es un superhéroe que viene de $this->planet y tiene los siguientes poderes: $powers";
+    return "$this->name es un superhéroe que viene de $this->planet y tiene los siguientes poderes: $this->powers";
+  }
+
+  public function level_up(){
+    $this->level++;
+    return "¡$this->name subió de nivel, al nivel $this->level!";
   }
 }
 
-$hero = new SuperHero();
-//echo $hero->name; // no muetra nada xq no tiene el constructor
-
-$hero->name = "Thor";
-$hero->powers = ["Superfuerza", "Volar", "Relámpago"];
-$hero->planet = "Asgard";
+$hero = new SuperHero("Thor", "Superfuerza, Volar y Relámpago", "Asgard");
 echo $hero->description();
-// intelephense marca 2 errores, pero se corrigen mas adelante en el video, el script se ejecuta correctamente
 ?>
