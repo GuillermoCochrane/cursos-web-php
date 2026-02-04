da5ef59a18e26cf2b31dbd0737b30f199c221e4a
GuillermoCochrane <guilleac81@gmail.com>
2026-02-04 04:27:07 -0300

    Agregado ejemplo de promoted properties y tipado estricto

--- midudev/clase 2/classes.php
+++ midudev/clase 2/classes.php
@@ -1,9 +1,10 @@
 <?php
+declare(strict_types=1);
 
 class SuperHero{
-  // propiedades y métodos
+/*   // propiedades y métodos
   public $name; // publico: podemos acceder a o modificar desde fuera de la clase
-  public $powers;
+  readonly public $powers; // readonly: solo podemos accederlo pero no modificarlo
   public $planet;
   private $level; // privado: solo podemos accederlo o modificarlo desde dentro de la clase, no desde fuera
 
@@ -14,21 +15,42 @@ class SuperHero{
     $this->planet = $planet;
     $this->level = 0;
   }
+ */
+
+  // promoted properties -> PHP 8
+  public function __construct(
+    public string $name,
+    public array $powers,
+    public string $planet,
+    private int $level = 0
+  ) {
+  }
 
   public function attack(){
     return "¡$this->name ataca con sus poderes!";
   }
 
   public function description(){
-    return "$this->name es un superhéroe que viene de $this->planet y tiene los siguientes poderes: $this->powers";
+    $powers = implode(", ", $this->powers); //similar al join en JS (concatena cada elemento del array con el siguiente, usando el delimitador que se le pase en el primer parametro)
+    return "$this->name es un superhéroe que viene de $this->planet y tiene los siguientes poderes: $powers";
   }
 
   public function level_up(){
     $this->level++;
-    return "¡$this->name subió de nivel, al nivel $this->level!";
+    return "¡Subio de nivel!  " . $this->show_current_level();
+  }
+
+  public function show_current_level(){
+    return "¡$this->name está en el nivel $this->level!";
+  }
+
+  public function show_all(){
+    return get_object_vars($this);
   }
 }
 
-$hero = new SuperHero("Thor", "Superfuerza, Volar y Relámpago", "Asgard");
+$hero = new SuperHero("Thor", ["Superfuerza", "Volar", "Relámpago"], "Asgard");
 echo $hero->description();
+echo "\n";
+echo $hero->level_up();
 ?>
