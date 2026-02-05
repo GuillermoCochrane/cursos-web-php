2ae337c383c9f3e934681bdb718ebeee05326374
GuillermoCochrane <guilleac81@gmail.com>
2026-02-05 20:52:29 -0300

    Agregado script de guardado de archivos en el server

--- Curso_PHP _Básico_jonmircha/clase_5_Subir_archivos/upload.php
+++ Curso_PHP _Básico_jonmircha/clase_5_Subir_archivos/upload.php
@@ -3,4 +3,10 @@
   foreach ($datos_archivo as $clave => $valor) {
     echo "Propiedad: $clave, => Valor: $valor, <br>";
   }
+
+  $archivo = $datos_archivo['tmp_name'];
+  $nombre_archivo = $datos_archivo['name'];
+  $destino = "uploaded/$nombre_archivo";
+  move_uploaded_file($archivo, $destino);
+  echo "<br> Archivo $nombre_archivo subido correctamente al servidor!!!";
 ?>
