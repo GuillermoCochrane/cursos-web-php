8aa85d3d921285a2c7078a40ae0f2f883b354149
GuillermoCochrane <guilleac81@gmail.com>
2026-02-05 20:57:01 -0300

    Agregada validacion de tipo de archivo (texto plano en este caso)

--- Curso_PHP _Básico_jonmircha/clase_5_Subir_archivos/upload.php
+++ Curso_PHP _Básico_jonmircha/clase_5_Subir_archivos/upload.php
@@ -6,7 +6,14 @@
 
   $archivo = $datos_archivo['tmp_name'];
   $nombre_archivo = $datos_archivo['name'];
+  $tipo_archivo = $datos_archivo['type'];
   $destino = "uploaded/$nombre_archivo";
-  move_uploaded_file($archivo, $destino);
-  echo "<br> Archivo $nombre_archivo subido correctamente al servidor!!!";
+
+  if ($tipo_archivo == "text/plain") {
+    move_uploaded_file($archivo, $destino);
+    echo "<br> Archivo $nombre_archivo subido correctamente al servidor!!!";
+  } else {
+    echo "Solo se permiten archivos de texto plano <br>";
+    echo "<a href='index.php'>Volver</a>";
+  }
 ?>
