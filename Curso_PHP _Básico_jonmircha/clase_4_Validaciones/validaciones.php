ddbf7d3b01abf4813f7abb78a36120ffaa4bdf0c
GuillermoCochrane <guilleac81@gmail.com>
2026-02-05 18:56:10 -0300

    Agregado ejemplo de sistema de login en php por post

--- Curso_PHP _Básico_jonmircha/clase_4_Validaciones/validaciones.php
+++ Curso_PHP _Básico_jonmircha/clase_4_Validaciones/validaciones.php
@@ -8,11 +8,24 @@
     $password_get = $_GET['password'];
     if ($nombre_get == $nombre && $password_get == $password) {
       echo "Formulario procesado por GET <br>";
-      echo "Hola $nombre_get <br>";
+      echo "Hola!! $nombre_get <br>";
       echo "Su contraseña es $password_get <br>";
       echo "y el sexo es " . $_GET['sexo'];
     } else {
       header("Location: index.php?error=true");
+    }
+  }
+
+  if (isset($_POST['hidden-post'])) {
+    $nombre_post = $_POST['nombre'];
+    $password_post = $_POST['password'];
+    if ($nombre_post == $nombre && $password_post == $password) {
+      echo "Formulario procesado por POST <br>";
+      echo "Hola!! $nombre_post <br>";
+      echo "Su contraseña es $password_post <br>";
+      echo "y el sexo es " . $_POST['sexo'];
+    } else {
+      header("Location: index.php?error_post=true");
     }
   }
 ?>
