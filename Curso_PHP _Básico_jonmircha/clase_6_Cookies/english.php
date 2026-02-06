dee38f380583dc84d9629574a7fe53f6caf10d38
GuillermoCochrane <guilleac81@gmail.com>
2026-02-06 05:38:38 -0300

    Destinos de las rutas  segun el valor de la cookie

---
+++ Curso_PHP _Básico_jonmircha/clase_6_Cookies/english.php
@@ -0,0 +1,4 @@
+<!-- destino si se elige inglés -->
+<h1>Hi, Welcome to my Website</h1>
+<br />
+<a href="delete_cookie.php">Delete Cookie</a>
---
+++ Curso_PHP _Básico_jonmircha/clase_6_Cookies/spanish.php
@@ -0,0 +1,4 @@
+<!-- destino si se elige español -->
+<h1>Hola, Bienvenido a mi página</h1>
+<br />
+<a href="delete_cookie.php">Borrar Cookie</a>
