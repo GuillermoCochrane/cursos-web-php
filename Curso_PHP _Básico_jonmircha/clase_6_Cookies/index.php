c9b65741fc19a2e9dd495284197e2be61a585fe9
GuillermoCochrane <guilleac81@gmail.com>
2026-02-06 05:06:02 -0300

    Archivo base para el ejemplo de la clase 6

---
+++ Curso_PHP _Básico_jonmircha/clase_6_Cookies/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cookies</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css" />
+</head>
+<body>
+  <h1>Cookies</h1>
+  <a href="save_cookie.php?language=es">Versión en español</a>
+    <br /><br />
+  <a href="save_cookie.php?language=en">Versión en inglés</a>
+</body>
+</html>
