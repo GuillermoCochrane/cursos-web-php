45b30b3e77d76af9e5491ff1b4eca682d7213268
GuillermoCochrane <guilleac81@gmail.com>
2026-02-08 21:54:30 -0300

    Agregadas hojas de estilos y middleware de session

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/components/head.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/components/head.php
@@ -2,3 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php echo APP_NAME; ?></title>
+<link rel="stylesheet" href="<?php echo APP_URL; ?>public/css/bulma.min.css">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>public/css/styles.css">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>public/css/sweetalert2.min.css">
+<script src="<?php echo APP_URL; ?>public/js/sweetalert2.all.min.js" ></script>
--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/index.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/index.php
@@ -2,8 +2,11 @@
 
     require_once "./config/app.php";
     require_once "./autoload.php";
+    require_once "./app/views/components/session_start.php";
 
-    $url_query_params = $_GET['views']; //es views, xq asi lo definimos al query params en .htaccess
+    $url_query_params = $_GET['views'] ?? ltrim($_SERVER['REQUEST_URI'] ?? '', '/');
+
+#    $url_query_params = $_GET['views'] ; //es views, xq asi lo definimos al query params en .htaccess
 
     if(isset($url_query_params)){
         $url=explode("/", $url_query_params); // explode es similar a slice en javascript
