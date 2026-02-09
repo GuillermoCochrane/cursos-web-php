12f6defabe62782067e903e5d42aae83c888c5f7
GuillermoCochrane <guilleac81@gmail.com>
2026-02-08 21:18:11 -0300

    Argegado teplate de home

---
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php require_once "./app/views/components/head.php"; ?>
+</head>
+<body>
+
+</body>
+    <h1>Ejercicio de CRUD en PHP y MySQL con POO, MVC y JS</h1>
+    <?php
+        require_once "./app/views/components/script.php";
+    ?>
+</html>
--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/index.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/index.php
@@ -3,12 +3,13 @@
     require_once "./config/app.php";
     require_once "./autoload.php";
 
-    $url_query_params = $_GET['views'];
+    $url_query_params = $_GET['views']; //es views, xq asi lo definimos al query params en .htaccess
 
     if(isset($url_query_params)){
         $url=explode("/", $url_query_params); // explode es similar a slice en javascript
     }else{
         $url=["login"];
     }
-
 ?>
+
+<?php require_once "./app/views/templates/home.php"; ?>
