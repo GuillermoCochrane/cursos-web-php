1525926a3adfc52bb2238d4d19886f1a90263204
GuillermoCochrane <guilleac81@gmail.com>
2026-02-09 17:07:26 -0300

    Agregada navBar

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/home.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/home.php
@@ -13,6 +13,7 @@
         if($vista=="login" || $vista=="404"){
             require_once "./app/views/templates/".$vista."-view.php";
         }else{
+            require_once "./app/views/components/navbar.php";
             require_once $vista;
         }
         require_once "./app/views/components/script.php";
