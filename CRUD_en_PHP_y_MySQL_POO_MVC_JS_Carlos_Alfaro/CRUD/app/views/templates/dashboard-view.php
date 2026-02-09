073c92996b40f0cd5918107063fca6e73c6645d5
GuillermoCochrane <guilleac81@gmail.com>
2026-02-08 22:46:39 -0300

    Implementado router basico

---
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/dashboard-view.php
@@ -0,0 +1,1 @@
+<h2>¡Bienvenidos al Dashboard!</h2>
--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/home.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/home.php
@@ -8,6 +8,16 @@
 </body>
     <h1>Ejercicio de CRUD en PHP y MySQL con POO, MVC y JS</h1>
     <?php
+        use app\controllers\viewsController;
+
+        $viewsController= new viewsController();
+        $vista=$viewsController->obtenerVistasControlador($url[0]);
+
+        if($vista=="login" || $vista=="404"){
+            require_once "./app/views/templates/".$vista."-view.php";
+        }else{
+            require_once $vista;
+        }
         require_once "./app/views/components/script.php";
     ?>
 </html>
