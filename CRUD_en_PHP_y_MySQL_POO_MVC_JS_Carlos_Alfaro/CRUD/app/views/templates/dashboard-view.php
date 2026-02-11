b6f9310cea5bf0df571fe4ec6107ca73f5d31eb7
GuillermoCochrane <guilleac81@gmail.com>
2026-02-11 04:23:34 -0300

    Corregido error en dashboard

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/dashboard-view.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/dashboard-view.php
@@ -1,13 +1,12 @@
 <div class="container is-fluid">
-	<h1 class="title">Home</h1>
-  	<div class="columns is-flex is-justify-content-center">
-    	<figure class="image is-128x128">
-    		<?php
-    				echo '<img class="is-rounded" src="'.APP_URL.'public/img/fotos/default.png">';
-    		?>
+	<div class="columns is-flex is-justify-content-center">
+		<figure class="image is-128x128">
+			<?php
+					echo '<img class="is-rounded" src="'.APP_URL.'public/img/fotos/default.png">';
+			?>
 		</figure>
-  	</div>
-  	<div class="columns is-flex is-justify-content-center">
-  		<h2 class="subtitle">¡Bienvenido <?php echo "Usuario"; ?>!</h2>
-  	</div>
+	</div>
+	<div class="columns is-flex is-justify-content-center">
+		<h2 class="subtitle">¡Bienvenido <?php echo "Usuario"; ?>!</h2>
+	</div>
 </div>
