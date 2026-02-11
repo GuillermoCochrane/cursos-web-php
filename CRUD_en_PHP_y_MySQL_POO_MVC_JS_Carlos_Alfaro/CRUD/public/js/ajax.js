34aecf81e2015cce99aeb04e18ac98c0863d8b32
GuillermoCochrane <guilleac81@gmail.com>
2026-02-11 04:03:18 -0300

    Agregada función que gestiona las acciones posteriores al envío de formularios en el front en ajax.js

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/public/js/ajax.js
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/public/js/ajax.js
@@ -47,3 +47,44 @@ formularios_ajax.forEach(formularios => { //recorremos todos los formularios
     });
 
 });
+
+function alertas_ajax(alerta){
+    if(alerta.tipo=="simple"){
+
+        Swal.fire({
+            icon: alerta.icono,
+            title: alerta.titulo,
+            text: alerta.texto,
+            confirmButtonText: 'Aceptar'
+        });
+
+    }else if(alerta.tipo=="recargar"){
+
+        Swal.fire({
+            icon: alerta.icono,
+            title: alerta.titulo,
+            text: alerta.texto,
+            confirmButtonText: 'Aceptar'
+        }).then((result) => {
+            if(result.isConfirmed){
+                location.reload();
+            }
+        });
+
+    }else if(alerta.tipo=="limpiar"){
+
+        Swal.fire({
+            icon: alerta.icono,
+            title: alerta.titulo,
+            text: alerta.texto,
+            confirmButtonText: 'Aceptar'
+        }).then((result) => {
+            if(result.isConfirmed){
+                document.querySelector(".FormularioAjax").reset();
+            }
+        });
+
+    }else if(alerta.tipo=="redireccionar"){
+        window.location.href=alerta.url;
+    }
+}
