0fc58f6a93dc07b3bc2cb04883e32ee94cfccaed
GuillermoCochrane <guilleac81@gmail.com>
2026-02-12 03:47:41 -0300

    Agregada validación de tipo de datos  al método del controlador de crear usuario

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/controllers/userController.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/controllers/userController.php
@@ -20,15 +20,63 @@
 			# Validación campos obligatorios #
 			if($nombre=="" || $apellido=="" || $usuario=="" || $clave1=="" || $clave2==""){
 				$alerta=[
-				"tipo"=>"simple",
-				"titulo"=>"Ocurrió un error inesperado",
-				"texto"=>"No has llenado todos los campos que son obligatorios",
-				"icono"=>"error"
-			];
-			return json_encode($alerta);
-					exit();
+					"tipo"=>"simple",
+					"titulo"=>"Ocurrió un error inesperado",
+					"texto"=>"No has llenado todos los campos que son obligatorios",
+					"icono"=>"error"
+				];
+				return json_encode($alerta);
+				exit();
 			}
 
+			# Validación de tipos de datos y largo de los mismos  para: #
+			//Nombre
+			if($this->verificarDatos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}",$nombre)){
+				$alerta=[
+					"tipo"=>"simple",
+					"titulo"=>"Ocurrió un error inesperado",
+					"texto"=>"El NOMBRE no coincide con el formato solicitado",
+					"icono"=>"error"
+				];
+				return json_encode($alerta);
+				exit();
+			}
+
+			//Apellido
+			if($this->verificarDatos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}",$apellido)){
+				$alerta=[
+					"tipo"=>"simple",
+					"titulo"=>"Ocurrió un error inesperado",
+					"texto"=>"El APELLIDO no coincide con el formato solicitado",
+					"icono"=>"error"
+				];
+				return json_encode($alerta);
+				exit();
+			}
+
+			// Usuario
+			if($this->verificarDatos("[a-zA-Z0-9]{4,20}",$usuario)){
+				$alerta=[
+					"tipo"=>"simple",
+					"titulo"=>"Ocurrió un error inesperado",
+					"texto"=>"El USUARIO no coincide con el formato solicitado",
+					"icono"=>"error"
+				];
+				return json_encode($alerta);
+				exit();
+			}
+
+			// Passwords y repetición
+			if($this->verificarDatos("[a-zA-Z0-9$@.-]{7,100}",$clave1) || $this->verificarDatos("[a-zA-Z0-9$@.-]{7,100}",$clave2)){
+				$alerta=[
+					"tipo"=>"simple",
+					"titulo"=>"Ocurrió un error inesperado",
+					"texto"=>"Las CLAVES no coinciden con el formato solicitado",
+					"icono"=>"error"
+				];
+				return json_encode($alerta);
+				exit();
+			}
 		}
 	}
 
