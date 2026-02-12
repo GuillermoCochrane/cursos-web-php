a40bde1f8e987564624f3c0272cd4ed731879a01
GuillermoCochrane <guilleac81@gmail.com>
2026-02-12 03:34:51 -0300

    Agregada validación de campos obligatorios al método del controlador de crear usuario

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/controllers/userController.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/controllers/userController.php
@@ -16,7 +16,18 @@
 			$email=$this->limpiarCadena($_POST['usuario_email']);
 			$clave1=$this->limpiarCadena($_POST['usuario_clave_1']);
 			$clave2=$this->limpiarCadena($_POST['usuario_clave_2']);
-
+			
+			# Validación campos obligatorios #
+			if($nombre=="" || $apellido=="" || $usuario=="" || $clave1=="" || $clave2==""){
+				$alerta=[
+				"tipo"=>"simple",
+				"titulo"=>"Ocurrió un error inesperado",
+				"texto"=>"No has llenado todos los campos que son obligatorios",
+				"icono"=>"error"
+			];
+			return json_encode($alerta);
+					exit();
+			}
 
 		}
 	}
