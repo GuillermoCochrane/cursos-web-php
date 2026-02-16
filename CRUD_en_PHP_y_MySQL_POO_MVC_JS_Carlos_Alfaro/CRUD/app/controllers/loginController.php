404c26228958457776885028bc4453418679eec3
GuillermoCochrane <guilleac81@gmail.com>
2026-02-16 05:37:31 -0300

    Agregada mètodo de cierre de session

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/controllers/loginController.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/controllers/loginController.php
@@ -84,5 +84,17 @@
 				}
 			}
 		}
+
+		
+		/*----------  Controlador cerrar sesion  ----------*/
+		public function cerrarSesionControlador(){
+			session_destroy();
+			
+			if(headers_sent()){
+				echo "<script> window.location.href='".APP_URL."login/'; </script>";
+			}else{
+				header("Location: ".APP_URL."login/");
+			}
+		}
 	}
 ?>
