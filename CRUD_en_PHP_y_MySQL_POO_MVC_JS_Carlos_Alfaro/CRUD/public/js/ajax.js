b797592bb6214a79be7716eeac3c8fad95476636
Guille <cochraneguillermo@gmail.com>
2026-05-14 18:22:10 -0300

    Agregado modal de confirmacion de logout

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/public/js/ajax.js
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/public/js/ajax.js
@@ -88,3 +88,28 @@ function alertas_ajax(alerta){
         window.location.href=alerta.url;
     }
 }
+
+/* Boton cerrar sesion */
+let btn_exit=document.getElementById("btn_exit");
+
+btn_exit.addEventListener("click", function(e){
+
+    e.preventDefault();
+    
+    Swal.fire({
+        title: '¿Quieres salir del sistema?',
+        text: "La sesión actual se cerrará y saldrás del sistema",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, salir',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let url=this.getAttribute("href");
+            window.location.href=url;
+        }
+    });
+
+});
