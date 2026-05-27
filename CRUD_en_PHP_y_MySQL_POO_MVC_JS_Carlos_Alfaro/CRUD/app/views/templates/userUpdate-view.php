1dd8c6dac4417d6c324e50da2946ff29eae942cc
Guille <cochraneguillermo@gmail.com>
2026-05-27 20:00:04 -0300

    Agregado encabezado opcional para edicion del usuario general

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userUpdate-view.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userUpdate-view.php
@@ -3,5 +3,8 @@
   <?php if($id==$_SESSION['id']){ ?>
     <h1 class="title">Mi cuenta</h1>
     <h2 class="subtitle">Actualizar cuenta</h2>
+  <?php }else{ ?>
+    <h1 class="title">Usuarios</h1>
+    <h2 class="subtitle">Actualizar usuario</h2>
   <?php } ?>
 </div>
