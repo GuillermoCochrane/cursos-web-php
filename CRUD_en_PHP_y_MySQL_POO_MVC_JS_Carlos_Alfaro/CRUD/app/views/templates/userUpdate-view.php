cfea39387cd46191d9ebd5fba4318a7dfd776311
Guille <cochraneguillermo@gmail.com>
2026-05-27 19:59:37 -0300

    Agregado encabezado opcional para edicion del usuario logueado

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userUpdate-view.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userUpdate-view.php
@@ -1,3 +1,7 @@
 <div class="container is-fluid mb-6">
 	<?php $id=$insLogin->limpiarCadena($url[1]);?>
+  <?php if($id==$_SESSION['id']){ ?>
+    <h1 class="title">Mi cuenta</h1>
+    <h2 class="subtitle">Actualizar cuenta</h2>
+  <?php } ?>
 </div>
