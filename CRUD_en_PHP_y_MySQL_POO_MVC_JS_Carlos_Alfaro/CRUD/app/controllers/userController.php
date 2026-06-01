92a495e45936d0e893abbda00692c1217d288e39
Guille <cochraneguillermo@gmail.com>
2026-06-01 20:12:25 -0300

    Verificamos integridad de los datos obligatorios

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/controllers/userController.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/controllers/userController.php
@@ -597,7 +597,95 @@
 			$email=$this->limpiarCadena($_POST['usuario_email']);
 			$clave1=$this->limpiarCadena($_POST['usuario_clave_1']);
 			$clave2=$this->limpiarCadena($_POST['usuario_clave_2']);
+
+			# Validammos los campos obligatorios
+			if($nombre=="" || $apellido=="" || $usuario==""){
+				$alerta=[
+					"tipo"=>"simple",
+					"titulo"=>"Ocurrió un error inesperado",
+					"texto"=>"No has llenado todos los campos que son obligatorios",
+					"icono"=>"error"
+				];
+				return json_encode($alerta);
+					exit();
+			}
+
+			# Verificamos integridad de los datos obligatorios
+
+			# Nombre
+			if($this->verificarDatos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}",$nombre)){
+				$alerta=[
+					"tipo"=>"simple",
+					"titulo"=>"Ocurrió un error inesperado",
+					"texto"=>"El NOMBRE no coincide con el formato solicitado",
+					"icono"=>"error"
+				];
+				return json_encode($alerta);
+					exit();
+			}
+
+			# Apellido
+			if($this->verificarDatos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}",$apellido)){
+				$alerta=[
+						"tipo"=>"simple",
+						"titulo"=>"Ocurrió un error inesperado",
+						"texto"=>"El APELLIDO no coincide con el formato solicitado",
+						"icono"=>"error"
+				];
+				return json_encode($alerta);
+					exit();
+			}
+
+			# Usuario 
+			if($this->verificarDatos("[a-zA-Z0-9]{4,20}",$usuario)){
+				$alerta=[
+					"tipo"=>"simple",
+					"titulo"=>"Ocurrió un error inesperado",
+					"texto"=>"El USUARIO no coincide con el formato solicitado",
+					"icono"=>"error"
+				];
+				return json_encode($alerta);
+				exit();
+			}
+
+			# Email
+			if($email!="" && $datos['usuario_email']!=$email){
+
+				if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+					$check_email=$this->ejecutarConsulta("SELECT usuario_email FROM usuario WHERE usuario_email='$email'");
+
+					if($check_email->rowCount()>0){
+
+						$alerta=[
+							"tipo"=>"simple",
+							"titulo"=>"Ocurrió un error inesperado",
+							"texto"=>"El EMAIL que acaba de ingresar ya se encuentra registrado en el sistema, por favor verifique e intente nuevamente",
+							"icono"=>"error"
+						];
+
+						return json_encode($alerta);
+						exit();
+					}
+
+					}else{
+
+					$alerta=[
+						"tipo"=>"simple",
+						"titulo"=>"Ocurrió un error inesperado",
+						"texto"=>"Ha ingresado un correo electrónico no valido",
+						"icono"=>"error"
+					];
+
+					return json_encode($alerta);
+					exit();
+				}
+			}
+
+
 		}
+
+		
 	}
 
 ?>
