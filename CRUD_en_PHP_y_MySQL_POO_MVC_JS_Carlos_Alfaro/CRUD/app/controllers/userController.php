10d875e4953f60d6a5ebbcdbdc7ea16aed1ee87a
Guille <cochraneguillermo@gmail.com>
2026-06-01 19:57:06 -0300

    Validamos nuevos los datos

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/controllers/userController.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/controllers/userController.php
@@ -588,6 +588,15 @@
 				return json_encode($alerta);
 				exit();
 			}
+
+			# Validamos nuevos los datos 
+			$nombre=$this->limpiarCadena($_POST['usuario_nombre']);
+			$apellido=$this->limpiarCadena($_POST['usuario_apellido']);
+
+			$usuario=$this->limpiarCadena($_POST['usuario_usuario']);
+			$email=$this->limpiarCadena($_POST['usuario_email']);
+			$clave1=$this->limpiarCadena($_POST['usuario_clave_1']);
+			$clave2=$this->limpiarCadena($_POST['usuario_clave_2']);
 		}
 	}
 
