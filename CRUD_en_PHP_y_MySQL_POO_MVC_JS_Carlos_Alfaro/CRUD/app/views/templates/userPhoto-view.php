eee99bc60da816817b6594ca797cc85f488dcb51
Guille <cochraneguillermo@gmail.com>
2026-06-02 13:06:13 -0300

    Si el usuario no existe, notificamos el error

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userPhoto-view.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userPhoto-view.php
@@ -1,4 +1,5 @@
 <div class="container is-fluid mb-6">
+  <!-- header dinamico -->
 	<?php $id=$insLogin->limpiarCadena($url[1]); ?>
 	<?php	if($id==$_SESSION['id']){ ?>
     <h1 class="title">Mi foto de perfil</h1>
@@ -10,6 +11,17 @@
 </div>
 <div class="container pb-6 pt-6">
 	<?php	
+    /* Boton de volver */
 		include "./app/views/inc/btn_back.php";
 		$datos=$insLogin->seleccionarDatos("Unico","usuario","usuario_id",$id);
   ?>
+  <!-- Si el usuario existe-->
+  <?php if($datos->rowCount()==1){ $datos=$datos->fetch(); ?>
+
+  <?php
+    /* Si el usuario no existe, notificar error */
+    }else {  
+      include "./app/views/inc/error_alert.php";
+    } 
+  ?>
+</div>
