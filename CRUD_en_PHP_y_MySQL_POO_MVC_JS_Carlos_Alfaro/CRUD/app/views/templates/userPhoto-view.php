856342f5b284f192786eabd3840f31b36e3b389e
Guille <cochraneguillermo@gmail.com>
2026-06-02 13:15:00 -0300

    Si el usuario existe Mostraos sus datos y comporamos si tiene imagen de perfil

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userPhoto-view.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userPhoto-view.php
@@ -15,8 +15,25 @@
 		include "./app/views/inc/btn_back.php";
 		$datos=$insLogin->seleccionarDatos("Unico","usuario","usuario_id",$id);
   ?>
-  <!-- Si el usuario existe-->
+  <!-- Si el usuario existe -->
   <?php if($datos->rowCount()==1){ $datos=$datos->fetch(); ?>
+  <!-- mostramos datos del usuario -->
+  <h2 class="title has-text-centered">
+    <?php echo $datos['usuario_nombre']." ".$datos['usuario_apellido']; ?>
+  </h2>
+
+	<p class="has-text-centered pb-6">
+    <?php echo "<strong>Usuario creado:</strong> ".date("d-m-Y  h:i:s A",strtotime($datos['usuario_creado']))." &nbsp; <strong>Usuario actualizado:</strong> ".date("d-m-Y  h:i:s A",strtotime($datos['usuario_actualizado'])); ?>
+  </p>
+
+  <div class="columns">
+		<div class="column is-two-fifths">
+      <!-- Comprobamos si tiene imagen de perfil -->
+      <?php if(is_file("./app/views/fotos/".$datos['usuario_foto'])){ ?>
+
+      <?php } ?>
+    </div>
+  </div>
 
   <?php
     /* Si el usuario no existe, notificar error */
