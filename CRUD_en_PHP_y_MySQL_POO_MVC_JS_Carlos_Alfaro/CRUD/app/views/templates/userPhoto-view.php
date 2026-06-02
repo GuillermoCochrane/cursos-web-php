bde6903d3e956c308ab997c7af3841b7ab1c8e47
Guille <cochraneguillermo@gmail.com>
2026-06-02 13:21:48 -0300

    Si tiene imagen de perfil, la mostramos junto con un boton para eliminarla

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userPhoto-view.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userPhoto-view.php
@@ -31,6 +31,21 @@
       <!-- Comprobamos si tiene imagen de perfil -->
       <?php if(is_file("./app/views/fotos/".$datos['usuario_foto'])){ ?>
 
+      <!-- Si tiene imagen de perfil, la mostramos junto con un boton para eliminarla -->
+      <figure class="image mb-6">
+        <img class="is-rounded" src="<?php echo APP_URL; ?>app/views/fotos/<?php echo $datos['usuario_foto']; ?>">
+			</figure>
+			
+			<form class="FormularioAjax" action="<?php echo APP_URL; ?>app/ajax/usuarioAjax.php" method="POST" autocomplete="off" >
+
+				<input type="hidden" name="modulo_usuario" value="eliminarFoto">
+				<input type="hidden" name="usuario_id" value="<?php echo $datos['usuario_id']; ?>">
+
+				<p class="has-text-centered">
+					<button type="submit" class="button is-danger is-rounded">Eliminar foto</button>
+				</p>
+			</form>
+
       <?php } ?>
     </div>
   </div>
