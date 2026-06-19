0f4317272d8100c2ad12c6d8205eb11a2868e855
Guille <cochraneguillermo@gmail.com>
2026-06-19 20:13:38 -0300

    Instanciamos el controlador de usuarios en el temaplte del controlador

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userSearch-view.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userSearch-view.php
@@ -1,1 +1,11 @@
-Buscar usuarios
+<div class="container is-fluid mb-6">
+    <h1 class="title">Usuarios</h1>
+    <h2 class="subtitle">Buscar usuarios</h2>
+</div>
+
+<div class="container pb-6 pt-6">
+  <?php
+      use app\controllers\userController;
+      $insUsuario = new userController();
+  ?>
+</div>
