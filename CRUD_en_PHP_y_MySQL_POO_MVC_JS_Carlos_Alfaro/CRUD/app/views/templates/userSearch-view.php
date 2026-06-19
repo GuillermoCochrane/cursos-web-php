161503b23d59717e4b8dfae20b42a9cf5447678b
Guille <cochraneguillermo@gmail.com>
2026-06-19 20:14:28 -0300

    Verificamos si se inicio una busqueda, y sino mostramos la intefaz para hacerlo

--- CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userSearch-view.php
+++ CRUD_en_PHP_y_MySQL_POO_MVC_JS_Carlos_Alfaro/CRUD/app/views/templates/userSearch-view.php
@@ -1,11 +1,34 @@
 <div class="container is-fluid mb-6">
-    <h1 class="title">Usuarios</h1>
-    <h2 class="subtitle">Buscar usuarios</h2>
+  <h1 class="title">Usuarios</h1>
+  <h2 class="subtitle">Buscar usuarios</h2>
 </div>
 
 <div class="container pb-6 pt-6">
   <?php
-      use app\controllers\userController;
-      $insUsuario = new userController();
+
+  use app\controllers\userController;
+
+  $insUsuario = new userController();
   ?>
+  <!-- header dinamico -->
+  <!-- Verificamos si se inicio una busqueda -->
+  <?php if (!isset($_SESSION[$url[0]]) && empty($_SESSION[$url[0]])) { ?>
+    <!-- Si no se inicio una busqueda, mostramos un boton para iniciarla -->
+    <div class="columns">
+      <div class="column">
+        <form class="FormularioAjax" action="<?php echo APP_URL; ?>app/ajax/buscadorAjax.php" method="POST" autocomplete="off">
+          <input type="hidden" name="modulo_buscador" value="buscar">
+          <input type="hidden" name="modulo_url" value="<?php echo $url[0]; ?>">
+          <div class="field is-grouped">
+            <p class="control is-expanded">
+              <input class="input is-rounded" type="text" name="txt_buscador" placeholder="¿Qué estas buscando?" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]{1,30}" maxlength="30" required>
+            </p>
+            <p class="control">
+              <button class="button is-info" type="submit">Buscar</button>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  <?php } ?>
 </div>
